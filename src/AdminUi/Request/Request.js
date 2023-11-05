a3d682978a1ad411d017c1edfc88e518fbcca9f9
Akminderjeet <92754926+Akminderjeet@users.noreply.github.com>
2023-11-05 12:52:21 +0530

    working on agent orders

--- src/AdminUi/Request/Request.js
+++ src/AdminUi/Request/Request.js
@@ -11,18 +11,7 @@ import { getRequest } from '../../api';
 import { useEffect } from 'react';
 import Button from '@mui/material/Button';
 import './Request.css'
-
-function createData(name, calories, fat, carbs, protein) {
-    return { name, calories, fat, carbs, protein };
-}
-
-const rows = [
-    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-    createData('Eclair', 262, 16.0, 24, 6.0),
-    createData('Cupcake', 305, 3.7, 67, 4.3),
-    createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
+import { acceptRequest, deleteRequest } from '../../api/adminApi';
 
 export default function BasicTable() {
     const [data, setdata] = useState([]);
@@ -47,8 +36,16 @@ export default function BasicTable() {
         setdata(roww);
         console.log(data);
     }
+    function accept(index) {
+        console.log("called");
+        acceptRequest(data[index]._id);
+    }
+    function deleteData(index) {
+        console.log("Delete Called");
+        deleteRequest(data[index]._id);
+    }
     return (
-        <TableContainer component={Paper} style={{ width: "100%", height: "100%" }}>
+        <TableContainer component={Paper} style={{ width: "100%", height: "92vh", overflowY: "scroll", overflowX: "hidden" }}>
             <Table sx={{ minWidth: 650 }} aria-label="simple table">
                 <TableHead>
                     <TableRow>
@@ -74,8 +71,8 @@ export default function BasicTable() {
                                 </TableCell>
                                 <TableCell align="right">{row.email}</TableCell>
                                 <TableCell align="right">{row.city}</TableCell>
-                                <TableCell align="right"><Button variant="contained">Delete</Button></TableCell>
-                                <TableCell align="right"><Button variant="contained">Accept</Button></TableCell>
+                                <TableCell align="right"><Button variant="contained" onClick={() => { deleteData(index) }}>Delete</Button></TableCell>
+                                <TableCell align="right"><Button variant="contained" onClick={() => { accept(index) }}>Accept</Button></TableCell>
                             </TableRow> :
                             <TableRow
                                 key={row.name}
---
+++ src/api/adminApi.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import axios, { AxiosError } from 'axios'
+
+const url = 'http://localhost:5000/';
+export const deleteRequest = async (id) => {
+    var tempUrl = url + "admin/deleteRequest";
+    return axios.patch(`${tempUrl}/${id}`, {
+        withCredentials: true
+    })
+}
+export const acceptRequest = async (id) => {
+    var tempUrl = url + "admin/acceptRequest";
+    return axios.patch(`${tempUrl}/${id}`, {
+        withCredentials: true
+    })
+}
